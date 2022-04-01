031c6db3e0e1cbbc7e1021cd0c0f88eda5ba1d28
Santosh Adhikari <npnewsb@gmail.com>
2022-04-01 11:19:41 +0545

    Validating Image and alt attribute value

--- src/test/java/swaglabs/inventory/ProductListsPageObject.java
+++ src/test/java/swaglabs/inventory/ProductListsPageObject.java
@@ -1,11 +1,20 @@
 package swaglabs.inventory;
 
 import net.serenitybdd.core.pages.PageObject;
+import org.openqa.selenium.By;
 
 import java.util.List;
 
 public class ProductListsPageObject extends PageObject {
     public List<String> titles() {
         return findAll(".inventory_item_name").textContents();
+    }
+
+    public void openProductDetailsFor(String itemName) {
+        find(By.linkText(itemName)).click();
+    }
+
+    public String imageTextForProduct(String productName) {
+        return $("//div[@class='inventory_item'][contains(.,'"+ productName +"')]//img").getAttribute("alt");
     }
 }
--- src/test/java/swaglabs/inventory/ViewingHighlightedProducts.java
+++ src/test/java/swaglabs/inventory/ViewingHighlightedProducts.java
@@ -3,6 +3,7 @@ package swaglabs.inventory;
 import net.serenitybdd.junit.runners.SerenityRunner;
 import net.thucydides.core.annotations.Managed;
 import net.thucydides.core.annotations.Steps;
+import org.assertj.core.api.SoftAssertions;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.openqa.selenium.WebDriver;
@@ -15,13 +16,16 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(SerenityRunner.class)
 public class ViewingHighlightedProducts {
-    @Managed
+
+    @Managed(driver = "chrome")
     WebDriver driver;
 
     @Steps
     LoginActions login;
 
     ProductListsPageObject productList;
+
+    ProductDetailsPageObject productDetails;
 
     @Test
     public void shouldDisplayHighlightedItemsOnWelcomePage() {
@@ -31,7 +35,32 @@ public class ViewingHighlightedProducts {
 
         assertThat(productsOnDisplay).hasSize(6)
                 .contains("Sauce Labs Backpack");
+    }
+
+    @Test
+    public void highlightedProductsShouldDisplayCorrespondingImages(){
+        login.as(User.STANDARD_USER);
+        List<String> productsOnDisplay = productList.titles();
+
+        SoftAssertions softAssertions = new SoftAssertions();
+        productsOnDisplay.forEach(
+                productName -> softAssertions.assertThat(productList.imageTextForProduct(productName)).isEqualTo(productName)
+        );
+
 
     }
 
+    @Test
+    public void shouldDisplayCorrectProductDetails() {
+        login.as(User.STANDARD_USER);
+
+        String firstItemName = productList.titles().get(0);
+
+        productList.openProductDetailsFor(firstItemName);
+
+        assertThat(productDetails.productName()).isEqualTo(firstItemName);
+        //Validating Img and alt value
+        productDetails.productImageWithAltValueOf(firstItemName).shouldBeVisible();
+
+    }
 }
